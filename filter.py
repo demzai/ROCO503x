0b1fd7be0acffa3b7d8bc20da26c05997b598cc2
demzai <demetrius.zaibo@students.plymouth.ac.uk>
2017-12-20 16:42:08 +0000

    Updated code
    Fixed dead reckoning and initialisation errors.
    Split filtering and dead reckoning sections.

--- filter.py
+++ filter.py
@@ -58,47 +58,12 @@ def getCol(data, column):
 
 # Filter a data set
 # filterX = [filterSelect, filterType, cutoffFrequency(ies), order]
-def filterData(data, filter1, filter2, sampleFrequency=imuSampleFrequency):
-    returnVal = [data[-1][0], 0,0,0, 0,0,0]
-
-    for x in range(1, 4):
-        returnVal[ x ] = doFilter(getCol(data, x  ), filter1[0],
-                                  filter1[1], np.array(filter1[2]),
-                                  filter1[3], sampleFrequency)[-1]
-        returnVal[x+3] = doFilter(getCol(data, x+3), filter2[0],
-                                  filter2[1], np.array(filter2[2]),
-                                  filter2[3], sampleFrequency)[-1]
+def filterData(data, columns, filter, sampleFrequency=imuSampleFrequency):
+    numColumns = len(columns)
+    returnVal = []
+    for column in range(0, numColumns):
+        returnVal += [doFilter(getCol(data, columns[column]), filter[0],
+                               filter[1], np.array(filter[2]),
+                               filter[3], sampleFrequency)[-1]]
     return returnVal
-
-
-# Filter a specific set of data
-def filterSensor(data, filter, sampleFrequency=imuSampleFrequency):
-    returnVal = [0,0,0]
-
-    for x in range(1, 4):
-        returnVal[ x ] = doFilter(getCol(data, x  ), filter[0],
-                                  filter[1], np.array(filter[2]),
-                                  filter[3], sampleFrequency)[-1]
-    return returnVal
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 

c8aae446d995f0929730d0a679b861ace33b7839
demzai <demetrius.zaibo@students.plymouth.ac.uk>
2017-12-22 21:58:45 +0000

    Added initial orientation estimation

--- Main.py
+++ Main.py
@@ -7,6 +7,8 @@ import filter as fl
 from pyqtgraph.Qt import QtCore
 import time
 import dead_reckoning as dr
+from math import *
+import quaternion as qt
 
 ####################################################
 ################# GLOBAL CONSTANTS #################
@@ -14,8 +16,8 @@ import dead_reckoning as dr
 inputType = "file"
 if (inputType == "live"):
     import Spatial_simple_cl as spatialC
-fileLocale = "UpDown2.txt"
-sleepTime = 0.0001
+fileLocale = "UpDown1.txt"
+sleepTime = 0.001
 graphWindow = gr.newWindow("Graphs", 640, 480)
 graphChart = gr.newGraph(graphWindow, "Test")
 graphAccX = gr.newPlot(graphChart, [], [], 'r', None, None, 3, 'o')
@@ -23,6 +25,7 @@ graphAccY = gr.newPlot(graphChart, [], [], 'g', None, None, 3, 'o')
 graphAccZ = gr.newPlot(graphChart, [], [], 'b', None, None, 3, 'o')
 updateEvery = 10
 cutoffFrequency = [20, 5]  # [Accel Low Pass, Gyro High Pass]
+startTime = 1
 
 ####################################################
 ################# GLOBAL VARIABLES #################
@@ -38,9 +41,9 @@ count = 0
 ####################################################
 ############## CALIBRATION CONSTANTS ###############
 ####################################################
-calGyroOffset  = [-0.35, -0.3, -0.45]
-calAccelOffset = [0.0, 0.0, 0.0]
-calAccelScale  = [1.0, 1.0, 1.0]
+calGyroOffset  = [-0.20854434285714282, -0.12702977142857147, -0.2672430000000005]
+calAccelOffset = [ 0.01725754327857845, -0.00160451736868370,  0.0145056265099980]
+calAccelScale  = [ 0.99893057626429155,  1.00137490276358830,  1.0021011922022352]
 
 
 ####################################################
@@ -60,16 +63,15 @@ def applyCalibration(data):
 
 # Retrieve the next input of raw data
 def getNextData():
-    """
-    # Extract the next piece of data
-    :param type: Determines whether the function tries to read from a file or directly from an IMU
-    :return:
-    """
     global inputType, imuObj
+    data = None
+
+    # If collecting data from a file then...
     if (inputType == "file"):
         # Considered an array of chars, so [:-1] removes the last character
         data = dataFile.readline()[:-1]
 
+    # Else if collecting data from the IMU in real time then...
     elif (inputType == "live"):
         # @todo get input directly from the IMU
         while (imuObj.dataReady == False):
@@ -77,6 +79,8 @@ def getNextData():
         if (imuObj.dataReady):
             data = imuObj.getData()
             #data = applyCalibration(data)
+
+    # Otherwise the user doesn't know what they want
     else:
         print("Error: invalid input type specified. Please set either \"file\" or \"live\"")
     # Try to convert the data into an array of floats
@@ -105,22 +109,40 @@ def init():
         imuObj = spatialC.IMU('some')
         time.sleep(1)
 
+    ###
+    # Initialise orientation
+    ###
+    # Scrap data until the start time
+    # [time, ax,  ay,  az,  gx,  gy,  gz]
+    while True:
+        tempList = getNextData()
+        if(tempList[0] > startTime):
+            break
 
+    # Assume IMU is stationary and derive initial orientation
+    # Reference: https://goo.gl/eChMxp
+    # [Roll, Pitch, Yaw]
+    orientation = [
+        atan2(tempList[2], tempList[3])*180/pi,
+        atan2(-tempList[1], sqrt(tempList[2]**2 + tempList[3]**2))*180/pi,
+        0.0
+    ]
+    quaternion = qt.euler_to_quat(orientation)
 
     ###
     # Initialise the lists
     ###
-    global listRaw, listCrude, listFiltered
+    global listRaw, listFiltered
     # [time, ax,  ay,  az,  gx,  gy,  gz]
     listRaw.append(getNextData())
 
     # [time, ax,  ay,  az,  vx,  vy,  vz,  px,  py,  pz,
     #   gx,  gy,  gz,  tx,  ty,  tz,  qw,  qx,  qy,  qz]
-    tempList = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
-    listRawDR.append(dr.doDeadReckoning(tempList, listRaw[0]))
+    tempList = [startTime, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    listRawDR.append(dr.doDeadReckoning(tempList+quaternion, listRaw[0]))
     listRawDR[0] = [listRawDR[0][0] - 0.004] + \
                    listRawDR[0][1:4] + [0.0, 0.0, 0.0, 0.0, 0.0, 0.0] + \
-                   listRawDR[0][4:7] + [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
+                   listRawDR[0][10:13] + [0.0, 0.0, 0.0] + listRawDR[0][16:20]
 
     listFiltered.append(listRaw[0])
     listFilteredDR.append(listRawDR[0])
@@ -179,13 +201,8 @@ def main():
     count = count % updateEvery
     if (inputType == 'file'):
         time.sleep(sleepTime)
-    if (inputType == 'live'):
-        time.sleep(sleepTime)
-    # gr.newPlot(graphChart, getCol(listRaw, 1)[-3:-1], getCol(listRaw, 0)[-3:-1], 'g', 'r', 'b', 5, 'o')
 
 
-
-# Cannot put into a function because QtCore.QTimer() is a dick
 # Initialize the system
 init()
 
